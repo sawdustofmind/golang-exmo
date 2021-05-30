ce254bef1d5f690b0886bd4edb2b58be12153d91
johny <johny@apple-admins-MacBook-Pro.local>
2021-05-30 13:10:56 +0300

    update pair settings fields

--- v1/pair_setting.go
+++ v1/pair_setting.go
@@ -3,12 +3,15 @@ package exmo
 import "net/url"
 
 type PairSettingStruct struct {
-	MinQuantity string `json:"min_quantity"`
-	MaxQuantity string `json:"max_quantity"`
-	MinPrice    string `json:"min_price"`
-	MaxPrice    string `json:"max_price"`
-	MaxAmount   string `json:"max_amount"`
-	MinAmount   string `json:"min_amount"`
+	MinQuantity     string `json:"min_quantity"`
+	MaxQuantity     string `json:"max_quantity"`
+	MinPrice        string `json:"min_price"`
+	MaxPrice        string `json:"max_price"`
+	MaxAmount       string `json:"max_amount"`
+	MinAmount       string `json:"min_amount"`
+	PricePrecision  int    `json:"price_precision"`
+	MakerCommission string `json:"commission_maker_percent"`
+	TakerCommission string `json:"commission_taker_percent"`
 }
 
 type PairSettings map[string]PairSettingStruct
